acc223294c57fa756320cc451dc835128b231b6e
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-01-19 10:21:26 -0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,7 @@ DEVICE_PATH := device/micromax/E7746
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
+TARGET_SUPPORTS_64_BIT_APPS := false.
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
