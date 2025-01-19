1d802471234f4116d234b92d3c352a4abf3df476
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-01-19 10:26:04 -0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ DEVICE_PATH := device/micromax/E7746
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
-TARGET_SUPPORTS_64_BIT_APPS := false.
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -32,10 +32,6 @@ TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
-
-
-TARGET_BOARD_SUFFIX := _64
-TARGET_USES_64_BIT_BINDER := true
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := E7746_64
